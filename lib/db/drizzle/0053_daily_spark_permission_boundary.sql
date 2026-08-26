d33915cacd23f48fddd94c559d9f860eec0fb649
brutisthebrut-debug <brutisthebrut@gmail.com>
2026-08-25 22:06:26 -0500

    feat(db): migrate Daily Spark permissions

---
+++ lib/db/drizzle/0053_daily_spark_permission_boundary.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "daily_spark_answers" ADD COLUMN "echo_use_allowed" boolean DEFAULT false NOT NULL;
+ALTER TABLE "daily_spark_answers" ADD COLUMN "echo_use_updated_at" timestamp;
+ALTER TABLE "daily_spark_answers" ADD COLUMN "learning_confirmed" boolean DEFAULT false NOT NULL;
+ALTER TABLE "daily_spark_answers" ADD COLUMN "learning_confirmed_at" timestamp;
+ALTER TABLE "daily_spark_answers" ADD COLUMN "matching_use_allowed" boolean DEFAULT false NOT NULL;
+ALTER TABLE "daily_spark_answers" ADD COLUMN "matching_use_updated_at" timestamp;
